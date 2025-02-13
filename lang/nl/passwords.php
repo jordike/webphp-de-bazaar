d2365fe0b50b05ee875d5628690de8c1f6817c0d
Jordi Keijzers <jordikeijzers@gmail.com>
2025-02-13 11:30:37 +0100

    feat: add Dutch localization for pagination, authentication, and password reset

---
+++ lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Uw wachtwoord is opnieuw ingesteld.',
+    'sent' => 'We hebben uw wachtwoord reset link gemaild.',
+    'throttled' => 'Wacht alstublieft voordat u het opnieuw probeert.',
+    'token' => 'Deze wachtwoord reset token is ongeldig.',
+    'user' => 'We kunnen geen gebruiker vinden met dat e-mailadres.',
+
+];
