cea5edba632ee2b35bec730347ccd5f2a2acc50a
Jordi Keijzers <jordikeijzers@gmail.com>
2025-02-18 14:22:55 +0100

    fix: reorder seeder calls in DatabaseSeeder for correct execution order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            RoleSeeder::class
+            RoleSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
