648939d73eb2ad04a8b3f487e58458d7e3790e50
Jordi Keijzers <jordikeijzers@gmail.com>
2025-04-02 11:39:09 +0200

    fix: update registration label in navigation to improve clarity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">
-                                    {{ __('auth.register.register') }}
+                                    {{ __('auth.registration.register') }}
                                 </a>
                             </li>
                         @endauth
