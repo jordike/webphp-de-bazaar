2c494e9e0182191c91e24f282142b7ba37637b7f
Jordi Keijzers <jordikeijzers@gmail.com>
2025-04-05 13:08:22 +0200

    feat: add ThemeMiddleware to handle theme management in requests

---
+++ app/Http/Middleware/ThemeMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Company;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ThemeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $company = $request->route('company');
+
+        if (is_int($company)) {
+            $company = Company::find($company);
+        }
+
+        $theme = $company->currentTheme ?? null;
+
+        if ($theme) {
+            session()->put('theme', $theme);
+        }
+
+        return $next($request);
+    }
+}
