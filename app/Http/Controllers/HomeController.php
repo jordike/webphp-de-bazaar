10a30ff2c6b3a81fc840e71dc6a014d638acd189
Jordi Keijzers <jordikeijzers@gmail.com>
2025-04-06 20:16:01 +0200

    fix: initialize rented and purchased products variables before use

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,14 +10,20 @@ class HomeController extends Controller
     public function __invoke()
     {
         $latestAdvertisements = Advertisement::latest()->take(3)->get();
-        $rentedProducts = auth()->user()
+
+        $rentedProducts = null;
+        $purchasedProducts = null;
+
+        if (auth()->check()) {
+            $rentedProducts = auth()->user()
             ->rentedProducts()
             ->with('advertisement')
             ->paginate(10);
-        $purchasedProducts = auth()->user()
+            $purchasedProducts = auth()->user()
             ->purchasedProducts()
             ->with('advertisement')
             ->paginate(10);
+        }
 
         return view('home', [
             'latestAdvertisements' => $latestAdvertisements,
