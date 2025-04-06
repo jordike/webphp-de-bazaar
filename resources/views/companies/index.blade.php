6dec20af2dfb475fcfd500999518d46cf44ffd09
Jordi Keijzers <jordikeijzers@gmail.com>
2025-04-06 21:54:21 +0200

    feat: implement localization for companies management views, enhancing user experience with translatable messages

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,20 +3,20 @@
 @section('title', 'Manage companies')
 
 @section('content')
-    <h1>Companies</h1>
+    <h1>{{ __('companies.companies') }}</h1>
 
     <x-status-messages />
 
-    @if($companies->isEmpty())
-        <p>No companies available.</p>
+    @if ($companies->isEmpty())
+        <p>{{ __('companies.no_companies_available') }}</p>
     @else
         <table class="table table-striped table-responsive table-bordered table-hover">
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
+                    <th>{{ __('companies.name') }}</th>
+                    <th>{{ __('companies.email') }}</th>
+                    <th>{{ __('companies.actions') }}</th>
                 </tr>
             </thead>
             <tbody>
@@ -26,13 +26,13 @@
                         <td>{{ $company->name }}</td>
                         <td>{{ $company->email }}</td>
                         <td>
-                            <a href="{{ route('companies.edit', $company->id) }}" class="btn btn-secondary btn-sm">Edit</a>
+                            <a href="{{ route('companies.edit', $company->id) }}" class="btn btn-secondary btn-sm">{{ __('companies.edit') }}</a>
 
                             <form action="{{ route('companies.destroy', $company->id) }}" method="POST" class="d-inline delete-form">
                                 @csrf
                                 @method('DELETE')
 
-                                <button type="submit" class="btn btn-danger btn-sm delete-button">Delete</button>
+                                <button type="submit" class="btn btn-danger btn-sm delete-button">{{ __('companies.delete') }}</button>
                             </form>
                         </td>
                     </tr>
