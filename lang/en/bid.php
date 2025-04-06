7ac27818dbe53ed6c144c5aab019c1c58db208a1
Jordi Keijzers <jordikeijzers@gmail.com>
2025-04-06 21:37:50 +0200

    feat: implement localization for bid messages and enhance bid creation view

---
+++ lang/en/bid.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'create_your_bid' => 'Create your bid',
+    'bid_amount' => 'Bid amount',
+    'bid_amount_placeholder' => 'Please enter your bid amount',
+    'submit_bid' => 'Submit bid',
+    'cancel' => 'Cancel',
+    'messages' => [
+        'max_bid_amount' => 'You can only place a maximum of 4 bids!',
+        'bid_placed' => 'Bid placed successfully!',
+        'bid_accepted' => 'Bid accepted successfully!',
+        'bid_rejected' => 'Bid rejected successfully!',
+    ]
+];
